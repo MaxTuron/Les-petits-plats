4aa4dabe1c301aca7cb737e0e3919e65f5f3feb6
MaxTuron <@=@4d~Yx|bAiu|`Yv!$N>
2022-04-19 15:37:44 +0200

    Mise en page recettes.

--- scripts/Class/recipesCard.js
+++ scripts/Class/recipesCard.js
@@ -13,20 +13,18 @@ class recipesInfos {
     }
 
     //CREATION DES CARTES DES RECETTES
-    creaCarteDom(){
+    creaCarteDom() {
         const article = document.createElement("article");
+        article.classList.add("recipeCards");
         article.innerHTML = `
         <p>ID: ${this.id}</p>
         <p>Nom: ${this.name}</p>
         <p>Personnes: ${this.servings}</p>
-        <p>${this.ingredients}</p> 
         <p>Temps: ${this.time}</p>
         <p>Description: ${this.description}</p>
+        <p>Outil: ${this.appliance}</p>
         <p>Ustensiles: ${this.ustensils}</p>
-        <p>---------------------------------------------------------------------------------------------------------</p>
-        `;
-
-
+     `;
         return article;
     }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -7,8 +7,11 @@ function displayRecettes (recipes) {
   
   recipes.forEach(recipe => {
     const recipeInfo = new recipesInfos(recipe);
+    
     const recipeCard = recipeInfo.creaCarteDom();
+
     recipesCard.appendChild(recipeCard);
+
   });
 }
 
